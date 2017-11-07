48097bc64a532c222032f764b2059e9fc77350e3
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-11-06 23:26:37 -0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="dateManager",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["dateRanges"],
+    url="https://github.com/VulcanoAhab/dateManager.git",
+    description="Date range helper for API[s] and Forms Edit",
+
+)
