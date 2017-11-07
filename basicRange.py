07759e38d04ef8b72583795d3bdf9f0ca2edfc9b
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-11-06 23:15:12 -0200

    First commit

---
+++ basicRange.py
@@ -0,0 +1,45 @@
+import calendar
+import datetime
+import collections
+
+class BaseRange:
+    """
+    """
+
+    _dateRange=0
+
+    def __init__(self, init, end, inFormat, outFormat=None):
+        """
+        """
+        self._inFormat=inFormat
+        self._outFormat=outFormat if outFormat else self._inFormat
+        self._init=datetime.datetime.strptime(init, self._inFormat)
+        self._end=datetime.datetime.strptime(end, self._inFormat)
+        self._maxDays=(self._end-self._init).days
+
+    def buildRanges(self):
+        """
+        """
+        sizis=self._dateRange
+        resultRange=collections.namedtuple("DateRange", ["init", "end"])
+        rangis=range(sizis,(self._end-self._init).days,sizis)
+        for n,week in enumerate(rangis):
+            nextDate=self._init+datetime.timedelta(days=week)
+            if n == 0:
+                init=self._init
+                end=nextDate
+            else:
+                init=nextDate
+                end=nextDate+datetime.timedelta(days=self._dateRange)
+            yield resultRange(init.strftime(self._outFormat), end.strftime(self._outFormat))
+
+class Weekly(BaseRange):
+    """
+    """
+
+    _dateRange=7
+
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        super().__init__(*args, **kwargs)
