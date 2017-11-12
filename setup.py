39f1b5f98ddc5e3a532492e3772ea3140d839944
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-11-12 14:01:29 -0200

    Add monthly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="dateManager",
-    version='0.1.0',
+    version='0.1.1',
     author="VulcanoAhab",
     packages=["dateRanges"],
     url="https://github.com/VulcanoAhab/dateManager.git",
